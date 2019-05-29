de8f8ceef61cc33d242b7821eb7d76a06f810c5c
Airam Trujillo Santos <airamtrs@gmail.com>
2019-05-29 20:48:32 +0100

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,12 @@ const pokemon1=document.querySelector('.pokemon1');
 const pokemon2=document.querySelector('.pokemon2');
 const result=document.querySelector('.result');
 button.addEventListener('click', function() {
-    const chosen=_.shuffle(pokemon).slice(0,2);
+    
+    const chosen=_.shuffle(pokemon).slice(0,2);  
     const namepokemon1=chosen[0].name.english;
     const namepokemon2=chosen[1].name.english;
-    const imagename1=/...namepokemon1+"*.png"}$/;
-    pokemon1.innerHTML=`<img src="img/${images[imagename1]}" alt="${namepokemon1}"></img>`; //No encuentra imagen
-    pokemon2.innerHTML=`<img src="img/${images[/.${namepokemon2.png}$/]}" alt="${namepokemon2}"></img>`;
+    pokemon1.innerHTML=`<img src="img/${images.chosen[0]}.png" alt="${namepokemon1}"></img>`; //No encuentra imagen
+    pokemon2.innerHTML=`<img src="img/${images.chosen[0]}.png" alt="${namepokemon2}"></img>`;
     const winner=Math.round(Math.random());
     result.textContent= 'El ganador es '+chosen[winner].name.english;
 });
